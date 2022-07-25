a1207dc5766e1d23289d91e83ab6ad45c4ea0ee3
Sugam Banskota <Sugambanskota123@gmail.com>
2022-07-25 09:17:52 -0500

    test-1 herokuUrl on axios.post

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -40,7 +40,7 @@ export const login = (email, password) => async (dispatch) => {
     }
 
     const { data } = await axios.post(
-      '/api/users/login',
+      'https://secure-ravine-48894.herokuapp.com/api/users/login',
       { email, password },
       config
     )
