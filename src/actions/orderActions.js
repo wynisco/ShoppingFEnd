7749e2ef8a299ecafb90602f9f0aee629a1c064a
Sugam Banskota <Sugambanskota123@gmail.com>
2022-07-25 00:00:56 -0500

    uses heroku url for backend call

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -22,6 +22,7 @@ import {
 } from '../constants/orderConstants'
 import { logout } from './userActions'
 
+const NetlifyUrl = "https://secure-ravine-48894.herokuapp.com";
 export const createOrder = (order) => async (dispatch, getState) => {
   try {
     dispatch({
@@ -39,7 +40,7 @@ export const createOrder = (order) => async (dispatch, getState) => {
       },
     }
 
-    const { data } = await axios.post(`/api/orders`, order, config)
+    const { data } = await axios.post(`${NetlifyUrl}/api/orders`, order, config)
 
     dispatch({
       type: ORDER_CREATE_SUCCESS,
@@ -81,7 +82,7 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
       },
     }
 
-    const { data } = await axios.get(`/api/orders/${id}`, config)
+    const { data } = await axios.get(`${NetlifyUrl}/api/orders/${id}`, config)
 
     dispatch({
       type: ORDER_DETAILS_SUCCESS,
@@ -123,7 +124,7 @@ export const payOrder = (orderId, paymentResult) => async (
     }
 
     const { data } = await axios.put(
-      `/api/orders/${orderId}/pay`,
+      `${NetlifyUrl}/api/orders/${orderId}/pay`,
       paymentResult,
       config
     )
@@ -164,7 +165,7 @@ export const deliverOrder = (order) => async (dispatch, getState) => {
     }
 
     const { data } = await axios.put(
-      `/api/orders/${order._id}/deliver`,
+      `${NetlifyUrl}/api/orders/${order._id}/deliver`,
       {},
       config
     )
@@ -204,7 +205,7 @@ export const listMyOrders = () => async (dispatch, getState) => {
       },
     }
 
-    const { data } = await axios.get(`/api/orders/myorders`, config)
+    const { data } = await axios.get(`${NetlifyUrl}/api/orders/myorders`, config)
 
     dispatch({
       type: ORDER_LIST_MY_SUCCESS,
@@ -241,7 +242,7 @@ export const listOrders = () => async (dispatch, getState) => {
       },
     }
 
-    const { data } = await axios.get(`/api/orders`, config)
+    const { data } = await axios.get(`${NetlifyUrl}/api/orders`, config)
 
     dispatch({
       type: ORDER_LIST_SUCCESS,
