e3ae9df573057c98272a507619aae78dc64b5e39
Sugam Banskota <Sugambanskota123@gmail.com>
2022-07-25 09:07:03 -0500

    testing herokuUrl on axios.get

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -53,7 +53,7 @@ export const listProductDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST })
 
-    const { data } = await axios.get(`/api/products/${id}`)
+    const { data } = await axios.get(`https://secure-ravine-48894.herokuapp.com/api/products/${id}`)
 
     dispatch({
       type: PRODUCT_DETAILS_SUCCESS,
