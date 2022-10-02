f9a625960ba3c95636f9c8765e721c0673d3dcb4
sachinrajgire <sachin.rajgire@mgmail.com>
2022-10-02 14:46:47 -0400

    running after building

---
+++ build/precache-manifest.03db4efa6ee95bc9c70956cbe974241d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c050009173fcddfb48e229b98248b76e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bd2bd449f3d3f65ae87",
+    "url": "/static/css/main.bb1cd6a4.chunk.css"
+  },
+  {
+    "revision": "06469d6f15ac3073f66d",
+    "url": "/static/js/2.3d76642d.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/static/js/2.3d76642d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd2bd449f3d3f65ae87",
+    "url": "/static/js/main.4402098d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.4402098d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1cbebc487047b8c078b34d27a4b11de6.js"
+  "/precache-manifest.03db4efa6ee95bc9c70956cbe974241d.js"
 );
 
 self.addEventListener('message', (event) => {
